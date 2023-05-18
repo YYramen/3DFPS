828d632fcb24558fcd36d262c77d78317915d54a
YusukeYoshida <87760101+YYramen@users.noreply.github.com>
2023-05-18 19:30:08 +0900

    Update: キャラの移動、カメラ挙動の作成

--- Assets/Scripts/Player/PlayerMove.cs
+++ Assets/Scripts/Player/PlayerMove.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    
+    [SerializeField, Tooltip("プレイヤーの移動速度")]
+    float _moveSpeed = 5f;
+
+    private Rigidbody _rb;
+    private float _horizontalInput;
+    private float _verticalInput;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        // 入力を受け付ける
+        _horizontalInput = Input.GetAxis("Horizontal");
+        _verticalInput = Input.GetAxis("Vertical");
+
+        // カメラの方向から、X-Z平面の単位ベクトルを取得
+        Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
+
+        // 方向キーの入力値とカメラの向きから、移動方向を決定
+        Vector3 moveForward = cameraForward * _verticalInput + Camera.main.transform.right * _horizontalInput;
+
+        // 移動方向にスピードを掛ける。ジャンプや落下がある場合は、別途Y軸方向の速度ベクトルを足す。
+        _rb.velocity = moveForward * _moveSpeed + new Vector3(0, _rb.velocity.y, 0);
+    }
 }
