10c7c694d3285a61f198bbce869a75f740ca71ba
YUSUKE <yoshidayusuke1124@gmail.com>
2023-06-11 14:42:43 +0900

    WIP: オブジェクトクラスを作成中

--- Assets/Scripts/Objects/Object.cs
+++ Assets/Scripts/Objects/Object.cs
@@ -1,6 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+
+public enum ObjectType
+{
+    None = 0,
+    NomalObj = 1,
+    GrapObj = 2,
+    PullObj = 3,
+}
 
 public class Object : MonoBehaviour
 {
@@ -13,25 +22,63 @@ public class Object : MonoBehaviour
     [SerializeField, Header("NomalObjのMaterial")]
     Material _nomalObjMaterials;
 
+    [SerializeField, Header("オブジェクトの種類")]
+    ObjectType _objType;
+
+    [Header("グラップオブジェクトの設定")]
+    [SerializeField, Header("プレイヤーを引っ張る力")]
+    float _grapPower;
+
+    [Header("プルオブジェクトの設定")]
+    [SerializeField, Header("オブジェクトを引っ張る力")]
+    float _pullPower;
+
+    [SerializeField, Header("Playerのオブジェクト")]
+    GameObject _playerObj;
+
+    [Tooltip("銃と接続しているかどうか")]
+    bool _isConnected = false;
+
+    public bool IsConnected { get => _isConnected; }
+
     private void Start()
     {
-        if(this.gameObject.layer == LayerMask.NameToLayer("GrapObject"))
-        {
-            Debug.Log($"{this.gameObject.name} のLayerは{this.gameObject.layer}です");
+        ObjectInitialize();
+    }
 
-            this.gameObject.GetComponent<Renderer>().material = _grapObjMaterials;
+    private void ObjectInitialize()
+    {
+        switch (_objType)
+        {
+            case ObjectType.None:
+                Debug.LogWarning($"<color=#00ffffff>{this.gameObject.name}にObjectType指定されていません</color>");
+                break;
+            case ObjectType.NomalObj:
+                this.gameObject.GetComponent<Renderer>().material = _nomalObjMaterials;
+                break;
+            case ObjectType.GrapObj:
+                this.gameObject.GetComponent<Renderer>().material = _grapObjMaterials;
+                break;
+            case ObjectType.PullObj:
+                this.gameObject.GetComponent<Renderer>().material = _pullObjMaterials;
+                break;
         }
-        else if(this.gameObject.layer == LayerMask.NameToLayer("PullObject"))
-        {
-            Debug.Log($"{this.gameObject.name} のLayerは{this.gameObject.layer}です");
+    }
 
-            this.gameObject.GetComponent<Renderer>().material = _pullObjMaterials;
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(_objType == ObjectType.None || _objType == ObjectType.NomalObj)
+        {
+            return;
         }
-        else if(this.gameObject.layer == LayerMask.NameToLayer("Object"))
-        {
-            Debug.Log($"{this.gameObject.name} のLayerは{this.gameObject.layer}です");
 
-            this.gameObject.GetComponent<Renderer>().material = _nomalObjMaterials;
+        if(_objType == ObjectType.GrapObj && collision.gameObject.layer == LayerMask.NameToLayer("GrapBullet"))
+        {
+
+        }
+        else if (_objType == ObjectType.PullObj && collision.gameObject.layer == LayerMask.NameToLayer("PullBullet"))
+        {
+
         }
     }
 }
