1c32670f08ff2cbf44c0fc9f2ee11b14f4c1bf62
YUSUKE <yoshidayusuke1124@gmail.com>
2023-06-12 19:13:15 +0900

    プロジェクトの更新（解決できないバグが出たため）

---
+++ Assets/Scripts/System/MouseCursorControll.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MouseCursorControll : MonoBehaviour
+{
+    [SerializeField, Header("RaycastÇÃRange")] float _raycastDistance = 50f;
+    [SerializeField, Header("è∆èÄÇÃImage")] Image _crosshairImage;
+
+    private void Update()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, _raycastDistance, LayerMask.GetMask("GrapObject")))
+        {
+            _crosshairImage.color = Color.blue;
+        }
+        else if ((Physics.Raycast(ray, out hit, _raycastDistance, LayerMask.GetMask("PullObject"))))
+        {
+            _crosshairImage.color = Color.red;
+        }
+        else
+        {
+            _crosshairImage.color = Color.white;
+        }
+    }
+}
